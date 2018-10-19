c2115aa67ad44ba24ba14c9e46b3381d70309a7d
ChloePark85 <hapark85@gmail.com>
2018-10-20 00:43:02 +0900

    모델 생성

--- community/models.py
+++ community/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    name=models.CharField(max_length=10)
+    place=models.TextField()
+    title=models.CharField(max_length=30)
+    contents=models.TextField()
+    tag=models.TextField()
+    cdate=models.DateTimeField(auto_now_add=True)
+    
